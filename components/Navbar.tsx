3666a4dce185bdc160e7d34b95cb909f504bcdde
Wafulah <wafulahvictor@gmail.com>
2023-07-11 21:42:18 -0700

    phase two

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,42 +3,45 @@ import Link from "next/link";
 import { NavLinks } from "../constants";
 import AuthProviders from "./AuthProviders";
 import { getCurrentUser } from "../lib/session";
+import { signOut } from "next-auth/react";
+
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
 
 const Navbar = async () => {
-    const session  = await getCurrentUser();
+  const session = await getCurrentUser()
+
   return (
-    <nav className="flexBetween navbar">
-      <div className="flex-1 flexStart gap-10">
-        <Link href="/">
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
           <Image
-            src="/public/logo.svg"
-            width={115}
+            src='/logo.svg'
+            width={116}
             height={43}
-            alt="Flexible"
+            alt='logo'
           />
         </Link>
-        <ul className="xl:flex hidden text-small gap-7">
+        <ul className='xl:flex hidden text-small gap-7'>
           {NavLinks.map((link) => (
-            <Link href={link.href} key={link.key}>
+            <Link href={link.href} key={link.text}>
               {link.text}
             </Link>
           ))}
         </ul>
       </div>
-      <div className="flexCenter gap-4">
-       {session?.user ?  (<>
-        {session?.user?.image && (<Image 
-           src={session.user.image}
-           width={40}
-           height={40}
-           className="rounded-full"
-           alt={session.user.name}
-        />)} 
-        <Link href="/create-project">
-            Share Work
-        </Link>
-        </>) : (
-            <AuthProviders />
+
+      <div className='flexCenter gap-4'>
+        {session?.user ? (
+          <>
+            <ProfileMenu session={session} />
+
+            <Link href="/create-project">
+              <Button title='Share work' />
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
         )}
       </div>
     </nav>
